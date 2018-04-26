5f6d27c673ac73cc92605d2fe8e3b667b93fd361
linxinzhe <linxinzhechn@qq.com>
2018-04-26 09:17:01 +0800

    feat(ch4): interceptor

--- src/main/java/top/linxz/java/spring/boot/action/springmvc/MyMvcConfig.java
+++ src/main/java/top/linxz/java/spring/boot/action/springmvc/MyMvcConfig.java
@@ -4,13 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import top.linxz.java.spring.boot.action.springmvc.interceptor.DemoInterceptor;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("top.linxz.java.spring.boot.action.springmvc")
-public class MyMvcConfig {
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -18,5 +22,21 @@ public class MyMvcConfig {
         viewResolver.setSuffix(". jsp");
         viewResolver.setViewClass(JstlView.class);
         return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("classpath:/assets/");
+    }
+
+    @Bean
+    public DemoInterceptor demoInterceptor() {
+        return new DemoInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(demoInterceptor());
     }
 }
